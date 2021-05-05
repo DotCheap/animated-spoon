a911a0fdc5445e0cdf503777816f10712b59c1eb
Anthony <tram_a@etna-alternance.net>
2021-05-05 17:30:50 +0200

    tests

---
+++ nodejs/test.js
@@ -0,0 +1,9 @@
+describe('My Test Suite', () => {
+    it('Test Case', () => {
+      expect(true).toEqual(true);
+    });
+    it('Expect', () => {
+      var a = 0
+      expect(a == 0)
+    })
+  });
